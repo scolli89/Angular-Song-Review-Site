c5981a8068165b451b620c86eb3bf4642edca484
Sean <35879707+scolli89@users.noreply.github.com>
2019-11-30 15:47:41 -0500

    I followed the we tutorial on authentiaction

--- AngularApp/lab5/src/app/home/home.component.ts
+++ AngularApp/lab5/src/app/home/home.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef,ViewChild } from '@angular/core';
+import { HttpService } from '../http.service';
+
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   
   clickCounter: number = 0;
   name: string = '';
-  
-  constructor() { }
+  @ViewChild('loginLine',{static:false}) loginLine: ElementRef;
+  @ViewChild('emailAddress',{static:false}) emailIn: ElementRef;
+  @ViewChild('pword',{static:false}) pwIn: ElementRef;
+
+  constructor(private http: HttpService) { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit(){
+
   }
 
   countClick(){
@@ -27,6 +37,40 @@ export class HomeComponent implements OnInit {
     };
     return myClasses;
   }
+
+  makeLogin(e,p,s){
+    console.log(e,p);
+   
+    if (e.style.display === "none") {
+      e.style.display = "inline";
+    } else {
+      e.style.display = "none";
+    }
+    if (p.style.display === "none") {
+      p.style.display = "inline";
+    } else {
+      p.style.display = "none";
+    }
+    
+
+  }
+  enterPE (e,p){
+    console.log(e.value);
+    console.log(p.value);
+    if(e.value != "" && p.value != ""){
+      console.log("sends")
+      // now call the http service to log in .
+
+    }
+
+
+  }
+
+  focusOutHandler(e){
+    
+    console.log(e.value);
+  }
+
 
 
 }
--- BackEnd/app.js
+++ BackEnd/app.js
@@ -8,8 +8,19 @@ const opn = require('./routes/open.route');
 const adm = require('./routes/admin.route');
 const secr = require('./routes/secure.route');
 
+const config = require("config");
+const usersRoute = require("./routes/user.route");
+
+
 const app = express();
-app.use(function(req, res, next) {
+
+//use config module to get the privatekey, if no private key set, end the application.a
+if(!config.get("myprivatekey")){
+    console.error("FATAL ERROR: myprivatekey is not defined.");
+    process.exit(1);
+}
+
+app.use(function(req, res, next) { // for cores ersuests
 
     res.header("Access-Control-Allow-Origin", "*");
   
@@ -19,28 +30,27 @@ app.use(function(req, res, next) {
   
     next();
   
-  });
+});
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://someuser:aTQILLzaSgd2lvOj@clusterlab5-9p81c.mongodb.net/test?retryWrites=true&w=majority";
 const mongoose = require('mongoose');
 mongoose.connect(uri,{
     useNewUrlParser: true,
-});
+}).then(()=>console.log('connected to the database(mongoose)'))
+.catch(err=> console.log("Could not connect to MongoDB..."));
 
-console.log('connected to the database(mongoose)');
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-//routes old
-// app.use('/api/songs', song);
-// app.use('/api/account',account);
-// app.use('/api/review',review);
+//tutorial route
+app.use("/api/users",usersRoute);
 //routes new
 app.use('/api/open',opn);
 app.use('/api/secure',secr);
 app.use('/api/admin',adm);
 
-let port = 8080;
+let port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 });
